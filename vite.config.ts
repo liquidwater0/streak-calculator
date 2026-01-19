b3bb13efb02917e77f41b544c4a5a34fb11ceb52
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2026-01-19 14:18:00 -0600

    make scss variables global

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/streak-calculator/"
+  base: "/streak-calculator/",
+  build: {
+		emptyOutDir: true,
+	},
+	resolve: {
+		alias: {
+			'@': `${path.resolve(__dirname, 'src')}`,
+			'@assets': `${path.resolve(__dirname, 'src/assets')}`,
+			'@scss': `${path.resolve(__dirname, 'src/scss')}`,
+			'@components': `${path.resolve(__dirname, 'src/components')}`,
+		},
+	},
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@scss/_colors.scss" as *;
+          @use "@scss/_font.scss" as *;
+          @use "@scss/_theme.scss" as *;
+        `
+      }
+    }
+  }
 });
