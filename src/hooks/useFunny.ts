947b7424ab4f5bb3bfdcd022122c56beb135f17f
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2026-01-19 15:18:50 -0600

    put funnies in effect just incase it breaks something

--- src/hooks/useFunny.ts
+++ src/hooks/useFunny.ts
@@ -1,11 +1,13 @@
-import "@scss/seriousStuff.scss";
+import { useEffect } from "react";
 import { isToday } from "date-fns";
 
 export default function useFunny() {
-    const isSeriousDay = isToday(new Date(new Date().getFullYear(), 3, 1));
-    const testing = false;
+    useEffect(() => {
+        const isSeriousDay = isToday(new Date(new Date().getFullYear(), 3, 1));
+        const testing = false;
 
-    if (isSeriousDay || testing) {
-        document.documentElement.dataset.serious = "true";
-    }
+        if (isSeriousDay || testing) {
+            document.documentElement.dataset.serious = "true";
+        }
+    }, []);
 }
